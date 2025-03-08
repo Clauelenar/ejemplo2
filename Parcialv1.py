cabe4707621632eb3f6a50d8a2b4c5cc9437d790
Clauelenar <160759625+Clauelenar@users.noreply.github.com>
2025-03-07 20:51:49 -0500

    Update Parcialv1.py

--- Parcialv1.py
+++ Parcialv1.py
@@ -16,6 +16,5 @@ def load_original_data():
         return None
 
 # Título de la app
-st.title("Análisis de Admisiones")
-data= pd.read_csv('https://raw.githubusercontent.com/Clauelenar/ejemplo2/refs/heads/main/university_student_dashboard_data.csv')
-data.head()
+st.title("Análisis de Admisiones.")
+
