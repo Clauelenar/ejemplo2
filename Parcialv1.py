96cd8c2c7fa3f964fe5f4f68fbffaf4031b1135c
Clauelenar <160759625+Clauelenar@users.noreply.github.com>
2025-03-07 20:50:24 -0500

    Update Parcialv1.py

--- Parcialv1.py
+++ Parcialv1.py
@@ -2,6 +2,19 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import requests
+from io import StringIO
+import streamlit as st
+
+def load_original_data():
+    url = 'https://raw.githubusercontent.com/Clauelenar/ejemplo2/refs/heads/main/university_student_dashboard_data.csv'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.read_csv(StringIO(response.text))
+    else:
+        st.error("Failed to load data from GitHub.")
+        return None
+
 # Título de la app
 st.title("Análisis de Admisiones")
 data= pd.read_csv('https://raw.githubusercontent.com/Clauelenar/ejemplo2/refs/heads/main/university_student_dashboard_data.csv')
